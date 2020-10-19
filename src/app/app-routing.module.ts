3a2dde8ee566ca940662afad2777eba35e3d6bfb
Christian00000 <c.froemmer@gmail.com>
2020-10-19 14:17:25 +0200

    linked the sites

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+const routes: Routes = [{
 
-const routes: Routes = [];
+  path: "", component: HomePageComponent
+  
+},
+  {
+    path: "about-us" , component: AboutUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
